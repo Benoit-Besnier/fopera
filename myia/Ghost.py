328f09d41622db452dcc6045d2ab13b00aa62fba
Benoit Besnier <Benoit-Besnier@users.noreply.github.com>
2019-04-03 13:37:38 +0200

    fix: Ghost should not sleep forever

--- myia/Ghost.py
+++ myia/Ghost.py
@@ -1,12 +1,9 @@
-from time import sleep
-
-from myia.Starter import Starter
+from myia.ia.GhostAI import GhostAI
 
 # Default playerId which is associated to the folder where server expect us to listen and answer
 player_id = 1
 
-
 def lancer():
-    # starter = Starter(player_id)
-    # starter.run()
-    sleep(10000000)
+    starter = Starter(player_id)
+    ai = GhostAI(player_id)
+    starter.run(ai)
